935abd22255aa4153cda25e1c062732b0ef7111b
kn <kn@email.email>
2025-02-11 13:56:00 -0500

    updates to config

---
+++ hyprpaper/.config/hypr/random-wallpaper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# define wallpaper diretory
+dir=~/Pictures/TSOTG
+
+# selects a random wallpaper from directory, -type f lists all files. shuf -n 1 randomly selects one file from the list
+
+background=$(find "$dir" -type f | shuf -n 1)
+
+# adding sleep to prevent race condition and let hyprpaper load first
+
+sleep 1
+
+# preload selected file & set preloaded image
+hyprctl hyprpaper preload "$background"
+hyprctl hyprpaper wallpaper " , $background"
+
+# pause briefly
+sleep 1
+
+# unload all unused
+hyprctl hyprpaper unload unused
